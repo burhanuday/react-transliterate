bf7418335ea078159268c5b1bcef5ebb20f9221a
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2020-09-25 19:24:18 +0530

    Updates

---
+++ precache-manifest.d4fdf53e9da83d8f842e13a3cf1df625.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb486132be5eb4060008fc3330e2fc24",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8f95c7482dcb8532436f",
+    "url": "./static/css/main.e2dfab96.chunk.css"
+  },
+  {
+    "revision": "100ea47fe3ec147474f6",
+    "url": "./static/js/2.7b7efcf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7b7efcf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f95c7482dcb8532436f",
+    "url": "./static/js/main.2de29040.chunk.js"
+  },
+  {
+    "revision": "69116dd46345a03785e6",
+    "url": "./static/js/runtime-main.ddb9e985.js"
+  }
+]);
