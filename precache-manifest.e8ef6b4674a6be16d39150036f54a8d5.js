36fed272ce3d63fbd5f4bdd33ccab1edd2bdd29c
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2021-05-10 21:11:09 +0530

    Updates

---
+++ precache-manifest.e8ef6b4674a6be16d39150036f54a8d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446ed919b646e8bde1886947d44f0c53",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ac8e974d89c964ae610",
+    "url": "./static/css/main.8338b857.chunk.css"
+  },
+  {
+    "revision": "291f7f7a6617a24629f4",
+    "url": "./static/js/2.6760ce1c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.6760ce1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac8e974d89c964ae610",
+    "url": "./static/js/main.5ca7325f.chunk.js"
+  },
+  {
+    "revision": "69116dd46345a03785e6",
+    "url": "./static/js/runtime-main.ddb9e985.js"
+  }
+]);
